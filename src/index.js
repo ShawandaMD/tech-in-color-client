ffec37900bb4403338a481ef943174e4c2920949
github username <github email address>
2019-12-20 13:03:03 -0500

    created store with dev tools

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import {Provider} from 'react-redux';
 import {connect} from 'react-redux';
 import {createStore} from 'redux';
 import thunk from 'redux-thunk';
+import rootReducer from ‘/reducers’;
+import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
 ReactDOM.render(
-  <Provider>
-    <App />
+  <Provider store={store} >
+    <App />,
   </Provider>
-  , document.getElementById('root'));
+  document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
