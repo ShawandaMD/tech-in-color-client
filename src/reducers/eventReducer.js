9a79a801a194bd19e9a0c410eddf5ca939304c5d
github username <github email address>
2019-12-22 12:32:34 -0500

    removed code

--- src/reducers/eventReducer.js
+++ src/reducers/eventReducer.js
@@ -1,8 +1,1 @@
-import userReducer from  './userReducer';
-import activitiyReducer from './activitiyReducer';
-import eventReducer from './eventReducer'
-import {combineReducers} from 'redux'
 
-export default combineReducers({userReducer,
-   activitiyReducer,
-   eventReducer})
