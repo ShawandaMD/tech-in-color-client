65e1970546a56c39ca74d62924baf6ea16b3632b
github username <github email address>
2019-12-25 22:11:54 -0500

    updated combineReducers with currentUser reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux'
 import activityReducer from './activityReducer'
 import eventReducer from './eventReducer'
 import userReducer from './userReducer'
+import currentUser from './currentUserReducer'
 
 export default combineReducers({
   activity: activityReducer,
   user: userReducer,
-  event: eventReducer
+  event: eventReducer,
+  currentUser
 })
 
 /*each key (user:)will point to one reducer (UserReducer)*/
