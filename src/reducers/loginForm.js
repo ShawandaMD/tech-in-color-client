e476c01efbb0c681516918e4e7d197758512bf47
github username <github email address>
2019-12-26 00:11:22 -0500

    created a reducer with initialState  as state

--- src/reducers/loginForm.js
+++ src/reducers/loginForm.js
@@ -1,2 +1,11 @@
 /*create a initial state because we dont want all the data*/
 const initialState = {email: "", password: ""}
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case ''
+      return
+    default:
+      return state
+
+  }
