31aa37762d3949f6b05c4ed8919750ca6dfe8795
github username <github email address>
2019-12-26 11:41:51 -0500

    imported loginForm Reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,12 +3,14 @@ import activityReducer from './activityReducer'
 import eventReducer from './eventReducer'
 import userReducer from './userReducer'
 import currentUser from './currentUserReducer'
+import loginForm from './loginForm
 
 export default combineReducers({
   activity: activityReducer,
   user: userReducer,
   event: eventReducer,
   currentUser
+  loginForm
 })
 
 /*each key (user:)will point to one reducer (UserReducer)*/
