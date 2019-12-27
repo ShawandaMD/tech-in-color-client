23e5cd18ff4ce292680b771ada9202e8447291bb
github username <github email address>
2019-12-27 13:06:36 -0500

    import connect and added state to value input in form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,14 @@
 import React from 'react';
-
+import {connect} from 'react-redux';
 
 /*Container component*/
 
-const Login = () => {
+const Login = (state) => {
 
   return (
     <form onSubmit={undefined}>
-      <input type="text" placeholder="Email" id="email" name="email" value={undefined} onChange={undefined}/>
-      <input type="text" placeholder="Password" id="password" name="password" value={undefined} onChange={undefined}/>
+      <input type="text" placeholder="Email" id="email" name="email" value={state.email} onChange={undefined}/>
+      <input type="text" placeholder="Password" id="password" name="password" value={state.password} onChange={undefined}/>
       <input type="submit" value="Log In"/>
     </form>
   )
