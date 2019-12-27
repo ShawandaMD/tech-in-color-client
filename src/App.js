4622a81867797ad025f118538b25a891ce8a1cf8
github username <github email address>
2019-12-27 12:14:49 -0500

    added login component to App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 /*import {connect} from 'react-redux';*/
 import './App.css';
+import Login from './components/Login.js'
+
 
 class App extends React.Component{
   render() {
     return (
-      "Hello World. I am react."
+      <Login />
     );
   }
 }
