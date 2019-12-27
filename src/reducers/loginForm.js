af12a93819db84abc9b56d08ec946578f0425c7f
github username <github email address>
2019-12-27 13:07:08 -0500

    tested out state using debugger-commented it out

--- src/reducers/loginForm.js
+++ src/reducers/loginForm.js
@@ -1,10 +1,11 @@
 /*create a initial state because we dont want all the data*/
 const initialState = {
-  email: "",
+  email: "THE",
   password: ""
 }
 
 export default (state = initialState, action) => {
+  /*debugger*/
   switch (action.type) {
     case 'NEW_LOGIN_FORM':
       return action.formData/*WHY????*/
