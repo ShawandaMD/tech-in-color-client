b46bb1434662d64b17fe910c573aefe7a777bc1c
github username <github email address>
2020-01-02 11:52:37 -0500

    added handleChange function

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import {newLoginForm} from '../actions/loginForm.js'
+import {updateLoginForm} from '../actions/loginForm.js'
 
 /*Container component*/
 
 const Login = (state) => {
 
+  const handleChange = event => {
+    /*this.setState.loginFormData({
+      [event.target.name]: event.target.value
+    })WHY DIDNT THIS WORK???*/
+    const updatedFormInfo = {
+     ...state.loginFormData,
+     [event.target.name]: event.target.value
+   }
+     state.updateLoginForm(updatedFormInfo)
+      }
+
   return (
     <form onSubmit={undefined}>
-      <input type="text" placeholder="Email" id="email" name="email" value={state.email} onChange={undefined}/>
-      <input type="text" placeholder="Password" id="password" name="password" value={state.password} onChange={undefined}/>
+      <input type="text" placeholder="Email" id="email" name="email" value={state.loginFormData.email} onChange={handleChange}/>
+      <input type="text" placeholder="Password" id="password" name="password" value={state.loginFormData.password} onChange={handleChange}/>
       <input type="submit" value="Log In"/>
     </form>
   )
@@ -17,9 +28,10 @@ const Login = (state) => {
 
 const mapStateToProps = (state) => {
   return {
-    email: state.loginForm.email,
-    password: state.loginForm.password
+    loginFormData: state.loginForm
+    /*email: state.loginForm.email,*/
+    /*password: state.loginForm.password*/
   }
 }
 
-export default connect(mapStateToProps)(Login)
+export default connect(mapStateToProps, {updateLoginForm})(Login)
