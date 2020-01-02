d681b186b11431af8138c95d9d73699e6a97741e
github username <github email address>
2020-01-02 11:51:55 -0500

    changed name of function to updateLoginForm

--- src/actions/loginForm.js
+++ src/actions/loginForm.js
@@ -1,6 +1,6 @@
-export const newLoginForm = (formData) => {
+export const updateLoginForm = (formData) => {
   return {
-    type: "NEW_LOGIN_FORM",
+    type: "UPDATE_LOGIN_FORM",
     formData
   }
 }
