0daa7eab5d0d534801e09f6209c3292feed89391
github username <github email address>
2020-01-10 13:29:15 -0500

    imported connect and getCurrentUser action

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-/*import {connect} from 'react-redux';*/
+import {connect} from 'react-redux';
+import {getCurrentUser} from '../acions/currentUser'
 import './App.css';
 import Login from './components/Login.js'
 
