8de0e35817c1e6254f1fafee8f91eb16c020a415
github username <github email address>
2020-01-15 18:17:45 -0500

    commented out console.log

--- src/reducers/currentUserReducer.js
+++ src/reducers/currentUserReducer.js
@@ -3,6 +3,7 @@ export default (state = null, action) => {
     case 'SET_CURRENT_USER':
       return action.user
     case 'CLEAR_CURRENT_USER':
+      //console.log(state)
       return null
     default:
       return state;
