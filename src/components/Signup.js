0a49d4d4ec3de27558347f33090671ba39da2839
github username <github email address>
2020-01-16 11:10:27 -0500

    imported signup and updateSingupForm actions

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import {updateSignupForm} from '../actions/signupForm.js'
+import {signup} from '../actions/currentUser.js'
 
 // container component
 
