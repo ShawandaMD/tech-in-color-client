ecb913accb997677ad0de4362011bf64233c80d4
github username <github email address>
2020-01-16 10:57:38 -0500

    created the form template

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -6,7 +6,16 @@ import {connect} from 'react-redux';
 const Signup = (props) => {
 
   return (
-
+    <form onSubmit={undefined}>
+      <input type="text" placeholder="Email" id="email" name="email" value={undefined} onChange={handleChange}/>
+      <input type="text" placeholder="Password" id="password" name="password" value={undefined} onChange={handleChange}/>
+      <input type="text" placeholder="First Name" id="f_name" name="f_name" value={undefined} onChange={handleChange}/>
+      <input type="text" placeholder="Last Name" id="l_name" name="l_name" value={undefined} onChange={handleChange}/>
+      <input type="text" placeholder="City" id="city" name="city" value={undefined} onChange={handleChange}/>
+      <input type="text" placeholder="State" id="state" name="state" value={undefined} onChange={handleChange}/>
+      <input type="number" placeholder="Zipcode" id="zip_code" name="zip_code" value={undefined} onChange={handleChange}/>
+      <input type="submit" value="Sign up"/>
+    </form>
   )
 }
 
