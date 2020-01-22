4ea35d7453697119ac24050159fdf50e38c6b54c
github username <github email address>
2020-01-21 21:42:12 -0500

    mapStateToProps for signup form data

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -7,18 +7,25 @@ import {signup} from '../actions/currentUser.js'
 
 const Signup = (props) => {
 
+
   return (
     <form onSubmit={undefined}>
-      <input type="text" placeholder="Email" id="email" name="email" value={undefined} onChange={handleChange}/>
-      <input type="text" placeholder="Password" id="password" name="password" value={undefined} onChange={handleChange}/>
-      <input type="text" placeholder="First Name" id="f_name" name="f_name" value={undefined} onChange={handleChange}/>
-      <input type="text" placeholder="Last Name" id="l_name" name="l_name" value={undefined} onChange={handleChange}/>
-      <input type="text" placeholder="City" id="city" name="city" value={undefined} onChange={handleChange}/>
-      <input type="text" placeholder="State" id="state" name="state" value={undefined} onChange={handleChange}/>
-      <input type="number" placeholder="Zipcode" id="zip_code" name="zip_code" value={undefined} onChange={handleChange}/>
-      <input type="submit" value="Sign up"/>
+      <input type="text" placeholder="Email" id="email" name="email" value={undefined} onChange={undefined}/>
+      <input type="text" placeholder="Password" id="password" name="password" value={undefined} onChange={undefined}/>
+      <input type="text" placeholder="First Name" id="f_name" name="f_name" value={undefined} onChange={undefined}/>
+      <input type="text" placeholder="Last Name" id="l_name" name="l_name" value={undefined} onChange={undefined}/>
+      <input type="text" placeholder="City" id="city" name="city" value={undefined} onChange={undefined}/>
+      <input type="text" placeholder="State" id="state" name="state" value={undefined} onChange={undefined}/>
+      <input type="number" placeholder="Zipcode" id="zip_code" name="zip_code" value={undefined} onChange={undefined}/>
+      <input type="submit" value="Sign Up"/>
     </form>
   )
+}
+
+const mapStateToProps = (state) => {
+  return {
+    signupFormData: state.signupForm
+  }
 }
 
 export default connect(mapStateToProps, {updateSignupForm, signup})(Signup)
