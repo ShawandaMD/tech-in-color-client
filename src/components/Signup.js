e67b8f1a356a77503016faddc6d71bce9c3c5768
github username <github email address>
2020-01-21 21:51:34 -0500

    added handleChange event listener

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -7,16 +7,23 @@ import {signup} from '../actions/currentUser.js'
 
 const Signup = (props) => {
 
+  const handleChange = event => {
+    const updatedFormInfo = {
+     ...props.signupFormData,
+     [event.target.name]: event.target.value
+   }
+     props.updateSignupForm(updatedFormInfo)
+      }
 
   return (
     <form onSubmit={undefined}>
-      <input type="text" placeholder="Email" id="email" name="email" value={undefined} onChange={undefined}/>
-      <input type="text" placeholder="Password" id="password" name="password" value={undefined} onChange={undefined}/>
-      <input type="text" placeholder="First Name" id="f_name" name="f_name" value={undefined} onChange={undefined}/>
-      <input type="text" placeholder="Last Name" id="l_name" name="l_name" value={undefined} onChange={undefined}/>
-      <input type="text" placeholder="City" id="city" name="city" value={undefined} onChange={undefined}/>
-      <input type="text" placeholder="State" id="state" name="state" value={undefined} onChange={undefined}/>
-      <input type="number" placeholder="Zipcode" id="zip_code" name="zip_code" value={undefined} onChange={undefined}/>
+      <input type="text" placeholder="Email" id="email" name="email" value={undefined} onChange={handleChange}/>
+      <input type="text" placeholder="Password" id="password" name="password" value={undefined} onChange={handleChange}/>
+      <input type="text" placeholder="First Name" id="f_name" name="f_name" value={undefined} onChange={handleChange}/>
+      <input type="text" placeholder="Last Name" id="l_name" name="l_name" value={undefined} onChange={handleChange}/>
+      <input type="text" placeholder="City" id="city" name="city" value={undefined} onChange={handleChange}/>
+      <input type="text" placeholder="State" id="state" name="state" value={undefined} onChange={handleChange}/>
+      <input type="number" placeholder="Zipcode" id="zip_code" name="zip_code" value={undefined} onChange={handleChange}/>
       <input type="submit" value="Sign Up"/>
     </form>
   )
