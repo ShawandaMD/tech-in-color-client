a3235dc7883150c0f91676ca940bc95331d4de32
github username <github email address>
2020-01-28 16:40:20 -0500

    imported router functionality from react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {connect} from 'react-redux';
 import './App.css';
-import Login from './components/Login.js'
-import Logout from './components/Logout.js'
-import {getCurrentUser} from './actions/currentUser.js'
-import Signup from './components/Signup.js'
+import Login from './components/Login.js';
+import Logout from './components/Logout.js';
+import {getCurrentUser} from './actions/currentUser.js';
+import Signup from './components/Signup.js';
+import { Route, Switch, withRouter } from 'react-router-dom'
 
 
 
