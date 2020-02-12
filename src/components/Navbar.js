7fea1f2936a1b5462ede3160115a8f8552a40e94
ShawandaMD <shawandadavis93@gmail.com>
2020-02-12 11:27:03 -0500

    returned users name if user is currentUser prop exists

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,15 +2,22 @@ import React from 'react';
 import {connect} from 'react-redux';
 import { NavLink } from 'react-router-dom'
 import Logout from './Logout'
+import Login from './Login'
 
 
-const Navbar = () => {
-  return()
+const Navbar = (props) => {
+  return(
+    <div>
+      {props.currentUser ? `Welcome ${props.currentUser.f_name}!` : ""}
+      {props.currentUser ? <Logout /> : <Login />}
+    </div>
+  )
 }
 
 const mapStateToProps = ({currentUser}) => {
-  currentUser,
-  loggedIn: !!currentUser
+  return {
+    currentUser
+  }
 }
 
 export default connect(mapStateToProps)(Navbar)
