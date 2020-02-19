982099d0087db66b64fbe51909872b505a67703b
ShawandaMD <shawandadavis93@gmail.com>
2020-02-19 13:22:54 -0500

    created variable to eventsData in mapStateToProps

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import {getEvents} from '../actions/events.js'
 
 const Events = (props) => {
-
+  debugger
   return(
     <div>
-      {props}
+      {props.getEvents()}
     </div>
   )
 }
 
 const mapStateToProps = (state) => {
-  debugger
   return {
-    state
+    eventsData: state.eventReducer
   }
 }
 
-export default connect(mapStateToProps)(Events)
+export default connect(mapStateToProps, {getEvents})(Events)
