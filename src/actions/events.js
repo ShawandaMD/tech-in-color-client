3a70bd468498aacdb9f53ab696501dabeb98f8a5
ShawandaMD <shawandadavis93@gmail.com>
2020-02-19 13:21:34 -0500

    added setEvents synchronous action creator

--- src/actions/events.js
+++ src/actions/events.js
@@ -1,26 +1,32 @@
+// SYNCHRONOUS ACTION CREATORS
+export const setEvents = (events) => {
+  return {
+    type: "SET_ALL_EVENTS",
+    events
+  }
+}
 
  // ASYNCHRONOUT ACTION CREATORS
 
- export const allEvents = () => {
-   return dispatch => {
-     return fetch("http://localhost:3001/events", {
-       credentials: "include",
-       method: 'GET',
-       headers: {
-         Accept: 'application/json',
-           "Content-Type": 'application/json'
-       }
-     })
-     .then((response) => response.json())
-     .then(events => {
-       if (events.error) {
-         alert(events.error)
-       } else {
-         dispatch(setEvents(events))
-       }
-     })
-     .catch(console.log)
-   }
+export const getEvents = () => {
+ return dispatch => {
+   return fetch("http://localhost:3001/events", {
+     credentials: "include",
+     method: 'GET',
+     headers: {
+       Accept: 'application/json',
+         "Content-Type": 'application/json'
+     }
+   })
+   .then((response) => response.json())
+   .then(events => {
+     debugger
+     if (events.error) {
+       alert(events.error)
+     } else {
+       dispatch(setEvents(events))
+     }
+   })
+   .catch(console.log)
  }
-   }
- }
+}
