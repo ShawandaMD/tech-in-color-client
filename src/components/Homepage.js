0adac34e217a9b0c6e00ea1765734fd7e5a754b0
ShawandaMD <shawandadavis93@gmail.com>
2020-02-19 13:41:32 -0500

    added links to login or logout

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 
 const Homepage = () => (
-  
+  <div>
+    <Link to="/login">log In</Link> or <Link to="/signup">Sign up!</Link>
+  </div>
 );
 
 export default Homepage;
