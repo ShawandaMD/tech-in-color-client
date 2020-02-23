292ec9c6c111fb096252bd47186042bd1763514c
ShawandaMD <shawandadavis93@gmail.com>
2020-02-23 14:45:11 -0500

    added exact routes for Login and Signup components

--- src/App.js
+++ src/App.js
@@ -22,6 +22,11 @@ class App extends React.Component{
       <div>
         {this.props.loggedIn ? <NavBar /> : <Homepage />}
         <Events />
+        <Switch>
+          <Route exact path='/login' component={Login}/>
+          <Route exact path='/signup'component={Signup}/>
+        </Switch>
+
 
       </div>
 
