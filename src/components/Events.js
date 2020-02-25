0f3eb222d63d162fd05738ad63edb0432c7429bb
ShawandaMD <shawandadavis93@gmail.com>
2020-02-25 15:09:57 -0500

    eventsCards variable to map through all events and display to page

--- src/components/Events.js
+++ src/components/Events.js
@@ -4,14 +4,15 @@ import {getEvents} from '../actions/events.js'
 
 const Events = (props) => {
   // debugger
-  return(
-    <div>
-      Hello
-    </div>
+  const eventCards = props.eventsData.map(e =>
+    <li key={e.id}>{e.title} by {e.organizer}</li>)
+  return (
+    <ul>{eventCards}</ul>
   )
 }
 
 const mapStateToProps = (state) => {
+  // debugger
   return {
     eventsData: state.eventReducer
   }
