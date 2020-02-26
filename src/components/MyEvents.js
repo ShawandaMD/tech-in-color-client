28b84dd16485bee959d730ee4c37a4428f1107f5
ShawandaMD <shawandadavis93@gmail.com>
2020-02-26 13:15:04 -0500

    added basic container component with imports and connecting map map state to props

---
+++ src/components/MyEvents.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {connect} from 'react-redux';
+
+const MyEvents = (props) => {
+  return(
+    <div> HELLO!!!</div>
+  )
+}
+
+const mapStateToProps = (state) => {
+  return {
+  
+  }
+}
+
+export default connect(mapStateToProps)(MyEvents)
