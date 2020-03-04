c6120fe1e673937441bce5694b62f8aad4b259d7
ShawandaMD <shawandadavis93@gmail.com>
2020-03-04 13:33:10 -0500

    added form to container component

--- src/components/EventsForm.js
+++ src/components/EventsForm.js
@@ -5,7 +5,14 @@ import {connect} from 'react-redux';
 const EventForm = (props) => {
 
   return (
-
+    <form onSubmit={undefined}>
+      <input type="text" placeholder="Title" id="title" name="title" value={undefined} onChange={undefined}/>
+      <input type="text" placeholder="Description" id="description" name="description" value={undefined} onChange={undefined}/>
+      <input type="date" placeholder="Date" id="date" name="date" value={undefined} onChange={undefined}/>
+      <input type="time" placeholder="Time" id="time" name="time" value={undefined} onChange={undefined}/>
+      <input type="link" placeholder="Link" id="link" name="link" value={undefined} onChange={undefined}/>
+      <input type="submit" value="Create Event"/>
+    </form>
   )
 }
 
