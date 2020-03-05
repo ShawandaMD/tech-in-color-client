d266f29fd82f967a5730b7d2958a564533714de4
ShawandaMD <shawandadavis93@gmail.com>
2020-03-05 12:51:25 -0500

    added redux dev tools and imported compose from redux

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import {createStore, applyMiddleware } from 'redux';
+import {createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 import './index.css';
@@ -9,10 +9,11 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const store = createStore(
   rootReducer,
-  applyMiddleware(thunk)
-  /*window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()*/
+  composeEnhancer(applyMiddleware(thunk))
 );
 
 ReactDOM.render(
